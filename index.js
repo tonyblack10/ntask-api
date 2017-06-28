a8a6abe08f471cc498414c779920239410ec20e8
Tony Costa <tonyblack10@gmail.com>
2017-06-28 18:37:15 -0300

    Construindo a API

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const consign = require('consign');
+
+const PORT = 3000;
+const app = express();
+
+app.set('json spaces', 4);
+
+consign()
+    .include('models')
+    .then('libs/middlewares.js')
+    .then('routes')
+    .then('libs/boot.js')
+    .into(app);
