9f6832cccd65608238477c67571c453d563d467e
Tony Costa <tonyblack10@gmail.com>
2017-06-28 21:07:12 -0300

    Implementando CRUD dos recursos da API

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,14 @@
+const bodyParser = require('body-parser');
+
 module.exports = app => {
     app.set('port', 3000);
     app.set('json spaces', 4);
+
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json());
+
+    app.use((req, res, next) => {
+        delete req.body.id;
+        next();
+    });
 };
